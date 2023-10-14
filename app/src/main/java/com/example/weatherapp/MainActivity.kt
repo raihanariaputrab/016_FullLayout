2c9b0d6ca64a03f708538e3e2f6603742580b75e
raihanariaputrab <arpb.business@gmail.com>
2023-10-14 11:40:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,222 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Home (){
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Text(
+            text = "WEATHER APP",
+            fontSize = 40.sp)
+        BoxHeader()
+        Spacer(modifier = Modifier.padding(20.dp))
+        Location()
+        Spacer(modifier = Modifier.padding(20.dp))
+        BoxBawah()
+    }
+}
+
+@Composable
+fun BoxHeader(){
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .clip(RoundedCornerShape(20.dp))
+        .background(Color.Cyan))
+    {
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()){
+            Image(
+                painter = painterResource(id = R.drawable.background),
+                contentDescription = "",
+                modifier = Modifier.size(200.dp)
+            )
+            Text(text = "Rain", fontSize = 25.sp, modifier = Modifier.padding(10.dp))
+        }
+    }
+}
+
+@Composable
+fun Location () {
+    Text(text = "20 C", fontSize = 30.sp, fontWeight = FontWeight.Bold)
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.baseline_location_on_24),
+            contentDescription = "", modifier = Modifier.size(40.dp)
+        )
+        Spacer(modifier = Modifier.padding(5.dp))
+        Text(text = "Yogyakarta", fontSize = 40.sp)
+    }
+    Text(
+        text = "Kasihan, Kabupaten Bantul Daerah Istimewa Yogyakarta",
+        textAlign = TextAlign.Center,
+        color = Color.Gray,
+    )
+}
+
+@Composable
+fun BoxBawah() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(20.dp))
+            .background(Color.Cyan)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Humidity",
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(10.dp)
+                )
+                Text(
+                    text = "UV Index",
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "98%",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(10.dp)
+                )
+                Text(
+                    text = "9 / 10",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Sunrise",
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(5.dp)
+                )
+                Text(
+                    text = "Sunset",
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(5.dp)
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "05.00 AM",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(5.dp)
+                )
+                Text(
+                    text = "05.40 PM",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(5.dp)
+                )
+            }
+        }
+    }
+}
+
+
+
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WeatherAppTheme {
+        Greeting("")
+    }
+    Home()
+}
